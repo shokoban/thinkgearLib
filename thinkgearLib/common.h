6709b2cc0d5271f53f06ab7b17c3e4b24076bc87
shokoban <shokoba.prog@gmail.com>
2021-03-21 13:13:52 +0900

    Fixed code to adjust to c lang and little modified.

--- thinkgearLib/common.h
+++ thinkgearLib/common.h
@@ -7,6 +7,7 @@
 #define THINKGEAR_COMMON_H
 
 
+/*** status of thinkgearLib protocol ***/
 #define TG_OK               0x00
 #define TG_NG               0xFF
 #define TG_CANNOT_CONNECT   0x40
@@ -29,58 +30,58 @@
 /*** structures for packet of thinkgearLib protocol ***/
 typedef struct tg_eeg_power_t
 {
-    float delta = 0;
-    float theta = 0;
-    float low_alpha = 0;
-    float high_alpha = 0;
-    float low_beta = 0;
-    float high_beta = 0;
-    float low_gamma = 0;
-    float mid_gamma = 0;
+    float delta;
+    float theta;
+    float low_alpha;
+    float high_alpha;
+    float low_beta;
+    float high_beta;
+    float low_gamma;
+    float mid_gamma;
 } tg_eeg_power_t;
 
 typedef struct tg_asic_eeg_power_t
 {
-    unsigned int delta = 0;
-    unsigned int theta = 0;
-    unsigned int low_alpha = 0;
-    unsigned int high_alpha = 0;
-    unsigned int low_beta = 0;
-    unsigned int high_beta = 0;
-    unsigned int low_gamma = 0;
-    unsigned int mid_gamma = 0;
+    unsigned int delta;
+    unsigned int theta;
+    unsigned int low_alpha;
+    unsigned int high_alpha;
+    unsigned int low_beta;
+    unsigned int high_beta;
+    unsigned int low_gamma;
+    unsigned int mid_gamma;
 } tg_asic_eeg_power_t;
 
 typedef struct tg_data_t
 {
-    unsigned char battery = 0;          // 0-127
-    unsigned char poor = 255;           // 0-255
-    unsigned char heart_rate = 0;       // 0-255
-    unsigned char attention = 0;        // 0-100
-    unsigned char meditation = 0;       // 0-100
-    unsigned char raw_wave_8bit = 0;    // 0-255
-    unsigned char raw_marker = 255;     // 0
-    int raw_wave = 0;                   /* a single big-endian 16-bit two's-compliment signed value
+    unsigned char battery;              // 0-127
+    unsigned char poor;                 // 0-255
+    unsigned char heart_rate;           // 0-255
+    unsigned char attention;            // 0-100
+    unsigned char meditation;           // 0-100
+    unsigned char raw_wave_8bit;        // 0-255
+    unsigned char raw_marker;           // 0
+    int raw_wave;                       /* a single big-endian 16-bit two's-compliment signed value
                                              (high-order byte followed by low-order byte) (-32768 to 32767) */
     tg_eeg_power_t eeg_power;           // eight big-endian 4-byte IEEE 754 floating point values
     tg_asic_eeg_power_t asic_eeg_power; // eight big-endian 3-byte unsigned integer values
-    unsigned int rr_interval = 0;       /* two byte big-endian unsigned integer
+    unsigned int rr_interval;           /* two byte big-endian unsigned integer
                                            representing the milliseconds between two R-peaks */
 } tg_data_t;
 
 typedef struct tg_flag_t
 {
-    unsigned char battery = 0;
-    unsigned char poor = 0;
-    unsigned char heart_rate = 0;
-    unsigned char attention = 0;
-    unsigned char meditation = 0;
-    unsigned char raw_wave_8bit = 0;
-    unsigned char raw_marker = 0;
-    unsigned char raw_wave = 0;
-    unsigned char eeg_power = 0;
-    unsigned char asic_eeg_power = 0;
-    unsigned char rr_interval = 0;
+    unsigned char battery;
+    unsigned char poor;
+    unsigned char heart_rate;
+    unsigned char attention;
+    unsigned char meditation;
+    unsigned char raw_wave_8bit;
+    unsigned char raw_marker;
+    unsigned char raw_wave;
+    unsigned char eeg_power;
+    unsigned char asic_eeg_power;
+    unsigned char rr_interval;
 } tg_flag_t;
 
 typedef struct tg_packet_t
